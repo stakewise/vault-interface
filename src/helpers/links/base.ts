3e88ea611b4fc074d79978a43727f26e779378e2
MikeDiam <info@mdiamond.ru>
2025-02-18 10:19:03 +0300

    [vault interface] update links

--- src/helpers/links/base.ts
+++ src/helpers/links/base.ts
@@ -4,19 +4,7 @@ import messages from '../messages/links'
 const base = {
   home: {
     path: '',
-    title: messages.restake,
-  },
-  withdraw: {
-    path: 'withdraw',
-    title: messages.withdraw,
-  },
-  rewards: {
-    path: 'rewards',
-    title: messages.rewards,
-  },
-  help: {
-    path: 'help',
-    title: messages.help,
+    title: messages.home,
   },
 }
 
