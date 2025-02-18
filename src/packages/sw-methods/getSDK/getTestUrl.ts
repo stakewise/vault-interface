6dbe93859cada8914c380308f6541ba7a2c1a5fd
MikeDiam <info@mdiamond.ru>
2025-02-18 14:22:07 +0300

    [vault interface] update sdk

--- src/packages/sw-methods/getSDK/getTestUrl.ts
+++ src/packages/sw-methods/getSDK/getTestUrl.ts
@@ -4,7 +4,8 @@ import * as constants from 'sw-helpers/constants'
 
 const getTestUrl = () => {
   const e2eCookieValue = cookie.get(constants.cookieNames.e2e)
-  const isTestUrlAllowed = !IS_PROD || IS_BEFORE_PROD
+  const isBeforeProd = typeof IS_BEFORE_PROD !== 'undefined' && IS_BEFORE_PROD
+  const isTestUrlAllowed = !IS_PROD || isBeforeProd
 
   if (isTestUrlAllowed && e2eCookieValue) {
     return 'http://localhost:8545'
