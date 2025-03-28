421a21ec7dcf69cf5949a4e2b22e3e47f2332fed
MikeDiam <info@mdiamond.ru>
2025-03-28 11:21:10 +0300

    [vault e2e] unboost tests

--- src/views/HomeView/content/util/useAPY.ts
+++ src/views/HomeView/content/util/useAPY.ts
@@ -51,8 +51,6 @@ const useAPY = ({ field, type }: Input) => {
     unboostExitingShares,
   } = useStore(storeSelector)
 
-  console.log({ userAPY, vaultAPY, maxBoostApy })
-
   const initialStateRef = useRef({
     newAPY: userAPY,
     isFetching: false,
