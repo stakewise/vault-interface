20215e038af9136868defac767ad5567ba2096ad
MikeDiam <info@mdiamond.ru>
2025-06-03 17:29:26 +0300

    [vault tokens] add comment

--- src/views/HomeView/StakeContext/util/stakeCtx.ts
+++ src/views/HomeView/StakeContext/util/stakeCtx.ts
@@ -64,6 +64,7 @@ export const {
   const { fetchExitQueue, claimExitQueue } = useExitQueue(vaultAddress)
   const { fetchUnboostQueue, claimUnboostQueue } = useUnboostQueue({ vaultAddress, fetchBalances })
 
+  // TODO
   const swapToken = swapTokens.selected
 
   const { fee, getBuyAmount, isFetching: isSwapQuoteFetching } = useSwapQuote({
