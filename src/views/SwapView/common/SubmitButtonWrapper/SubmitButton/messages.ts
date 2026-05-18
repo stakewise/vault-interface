aae7d835a11ffaf9c7514d9105ca7969fa79a67e
dfkadyr <dfkadyr@gmail.com>
2026-05-18 10:56:24 +0300

    SubmitButton readOnly

---
+++ src/views/SwapView/common/SubmitButtonWrapper/SubmitButton/messages.ts
@@ -0,0 +1,11 @@
+export default {
+  readOnlyTitle: {
+    en: 'Read-only mode',
+    ru: 'Режим только для чтения',
+    fr: 'Mode lecture seule',
+    es: 'Modo solo lectura',
+    pt: 'Modo somente leitura',
+    de: 'Nur-Lese-Modus',
+    zh: '只读模式',
+  },
+}
